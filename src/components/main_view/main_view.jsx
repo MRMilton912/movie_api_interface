bcef2f5cf2afc6021a5be7355e4def1dee57208c
MRMilton912 <multimillionairemilton@gmail.com>
2023-09-09 04:45:40 -0400

    Initial commit

---
+++ src/components/main_view/main_view.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
+export const MainView = () => {
+  const [books, setMovie] = useState([
+    {
+      id: 1,
+      title: "The Departed'",
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
+      Director: "Martin Scorsese"
+    },
+    {
+      id: 2,
+      title: "The Wolf of Wall Street",
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/51WAikRq37L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+      Director: "Martin Scorsese"
+    },
+    {
+      id: 3,
+      title: "Casino",
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX381_BO1,204,203,200_.jpg",
+      Director: "Martin Scorsese"
+    },
+    {
+      id: 4,
+      title: "Kill Bill",
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/51HbNW6RzhL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+      Director: "Quentin Tarantino"
+    },
+    {
+      id: 5,
+      title: "Django Unchained",
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/41MBLi5a4jL._SX384_BO1,204,203,200_.jpg",
+      Director: "Quentin Tarantino"
+    }
+  ]);
+
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  if (selectedMovie) {
+    return <MovieView movie={selectedMovie} />;
+  }
+
+  if (movie.length === 0) {
+    return <div>The list is empty!</div>;
+  }
+
+  return (
+    <div>
+      {movie.map((book) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onClick={() => {
+            setSelectedMovie(movie);
+          }}
+        />
+      ))}
+    </div>
+  );
+};
