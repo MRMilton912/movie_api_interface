cb060d07121915c403ac259177af617dc06c29b3
MRMilton912 <multimillionairemilton@gmail.com>
2023-10-27 12:57:03 -0400

    Stash

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -2979,7 +2979,7 @@ $RefreshReg$(_c, "App");
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-dev-runtime":"iTorj","react-dom/client":"lOjBx","./components/main-view/main-view":"4gflv","./index.scss":"lJZlQ","@parcel/transformer-js/src/esmodule-helpers.js":"7dr0g","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"gQySZ","bootstrap/dist/css/bootstrap.min.css":"i5LP7","react-bootstrap/Container":"hEdsw"}],"iTorj":[function(require,module,exports) {
+},{"react/jsx-dev-runtime":"iTorj","react-dom/client":"lOjBx","./components/main-view/main-view":"4gflv","./index.scss":"lJZlQ","@parcel/transformer-js/src/esmodule-helpers.js":"7dr0g","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"gQySZ","react-bootstrap/Container":"hEdsw","bootstrap/dist/css/bootstrap.min.css":"i5LP7"}],"iTorj":[function(require,module,exports) {
 "use strict";
 module.exports = require("ee51401569654d91");
 
